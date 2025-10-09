8438c4ad3774e20f392868caac460070a9141850
Arturo Espinoza <arturo.espi112@gmail.com>
2025-10-09 09:28:12 -0500

    ejercicio pandas con json

--- ejercicios_en_Clase/pandas-json.py
+++ ejercicios_en_Clase/pandas-json.py
@@ -4,3 +4,6 @@ import pandas as pd
 df=pd.read_json("data/personas.json")
 #imprimir el dataframe
 print(df)
+#normalizar el json
+f_normalizado = pd.json_normalize(df['Estudios'])
+#df_normalizado = pd.concat([df.drop(columns="Estudios")])
